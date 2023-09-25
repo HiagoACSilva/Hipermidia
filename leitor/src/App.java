8ff457cb35719bcb955895a8ed9022ea6491435b
Hiago Antonio Costa Silva <71812623+HiagoACSilva@users.noreply.github.com>
2023-09-25 19:07:15 -0300

    Adicionando Hash
    Adicionando o array Hash cache

--- leitor/src/App.java
+++ leitor/src/App.java
@@ -1,74 +1,106 @@
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.*;
-import java.util.Scanner;
-import java.util.regex.Pattern;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
 public class App {
     private static final String FILENAME = "src\\verbetesWikipedia.xml";
-
+    private static HashMap<String, List<Pagina>> cache = new HashMap<>();
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         // Instantiate the Factory
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        String pesquisa;
         System.out.println("Digite o titulo");
-        String pesquisa = scan.nextLine();
-        try {
-
-            // parse XML file
-            DocumentBuilder db = dbf.newDocumentBuilder();
-
-            Document doc = db.parse(new File(FILENAME));
-            NodeList list = doc.getElementsByTagName("page");
-            List<Pagina> paginas = new ArrayList<>();
-            for (int temp = 0; temp < list.getLength(); temp++) {
-                Node node = list.item(temp);
-
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-
-                    Element element = (Element) node;
-                    // get text
-                    String id = element.getElementsByTagName("id").item(0).getTextContent();
-                    String title = element.getElementsByTagName("title").item(0).getTextContent();
-                    String text = element.getElementsByTagName("text").item(0).getTextContent();
-                    if (text.toLowerCase().contains(pesquisa.toLowerCase())) {
-                        long count = Pattern.compile(pesquisa, Pattern.CASE_INSENSITIVE).matcher(text).results()
-                                .count();
-                        long points = count;
-                        if (title.toLowerCase().contains(pesquisa.toLowerCase())) {
-                            points += 10;
-                        }
-                        paginas.add(new Pagina(id, title, count, points));
+        do{
+            pesquisa = scan.nextLine();
+            if(cache.get(pesquisa) != null){
+                for (Pagina pagina : cache.get(pesquisa)) {
+                    if (pagina.getPoints() > 50) {
+                        
+                        System.out.println("Id: " + pagina.getId() + ", Titulo: " + pagina.title + ", QdR: "
+                                + pagina.getQdR());
                     }
-
                 }
+            }else{
 
-            }
-            if (!paginas.isEmpty()) {
-                Comparator<Pagina> relevancia = Collections.reverseOrder(Comparator.comparing(Pagina::getPoints));
-                Collections.sort(paginas, relevancia);
-            }
-            for (Pagina pagina : paginas) {
-                if (pagina.getPoints() > 50) {
-                    System.out.println("Id: " + pagina.getId() + ", Titulo: " + pagina.title + ", QdR: "
-                            + pagina.getQdR());
+            //PESQUISA
+            
+                try {
+
+                    // parse XML file
+                    DocumentBuilder db = dbf.newDocumentBuilder();
+
+                    Document doc = db.parse(new File(FILENAME));
+                    NodeList list = doc.getElementsByTagName("page");
+                    List<Pagina> paginas = new ArrayList<>();
+                    for (int temp = 0; temp < list.getLength(); temp++) {
+                        Node node = list.item(temp);
+
+                        if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+                            Element element = (Element) node;
+                            // get text
+                            String id = element.getElementsByTagName("id").item(0).getTextContent();
+                            String[] title = element.getElementsByTagName("title").item(0).getTextContent().split(" ");
+                            String[] text = element.getElementsByTagName("text").item(0).getTextContent().toLowerCase().split(" ");
+                            
+                            for (String string : title) {
+                                if(cache.get(string) == null){
+                                    cache.put(string, paginas);
+                                }
+                            }
+                            for (String string : text) {
+                                
+                            }
+                            //Busca
+                           /*   if (text.toLowerCase().contains(pesquisa.toLowerCase())) {
+                                long count = Pattern.compile(pesquisa, Pattern.CASE_INSENSITIVE).matcher(text).results()
+                                        .count();
+                                long points = count;
+                                if (title.toLowerCase().contains(pesquisa.toLowerCase())) {
+                                    points += 10;
+                                }
+                                paginas.add(new Pagina(id, title, count, points));
+                            }*/
+
+                        }
+
+                    }
+                    if (!paginas.isEmpty()) {
+                        Comparator<Pagina> relevancia = Collections.reverseOrder(Comparator.comparing(Pagina::getPoints));
+                        Collections.sort(paginas, relevancia);
+                        cache.put(pesquisa, paginas);
+                        for (Pagina pagina : cache.get(pesquisa)) {
+                            if (pagina.getPoints() > 50) {
+                                
+                                System.out.println("Id: " + pagina.getId() + ", Titulo: " + pagina.title + ", QdR: "
+                                        + pagina.getQdR());
+                            }
+                        }
+                    }
+                    
+                } catch (ParserConfigurationException | SAXException | IOException e) {
+                    e.printStackTrace();
                 }
             }
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            e.printStackTrace();
-        }
+            System.out.println("Nova Pesquisa?");
+        }while(pesquisa != "sair");
         scan.close();
 
     }
@@ -105,6 +137,6 @@ public class App {
         public long getQdR() {
             return qdR;
         }
-
     }
+
 }
