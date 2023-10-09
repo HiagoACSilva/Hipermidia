5deb288c5488a55472901f87562e4af5d57434b1
Hiago Antonio Costa Silva <71812623+HiagoACSilva@users.noreply.github.com>
2023-10-09 01:30:55 -0300

    Adicionando o sistema de cache
    Adicionando o sistema de cache, porem está dando erro na linha 50, o erro é "out of bounds for length" se pesquisa alguma palavra que nao existe ou não foi calculada
    ainda resta fazer:
    - Se colocar uma palavra invalida ou sem resultados, dizer que não há resultados
    - Adicionar a funcionalidade de pesquisa com mais strings de uma vez só

--- leitor/src/App.java
+++ leitor/src/App.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Scanner;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -19,111 +18,133 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class App {
-    private static final String FILENAME = "src\\verbetesWikipedia.xml";
-    private static HashMap<String, List<Pagina>> cache = new HashMap<>();
+    private static final String FILENAME = "verbetesWikipedia.xml";
+    private static HashMap<Integer, Pagina> Paginas = new HashMap<>(); // CACHE DE UM HASHMAP EM QUE A <STRING, LIST>
+                                                                       // SERIA STRING PESQUISA E LIST DE PAGINAS DESSA
+                                                                       // PESQUISA
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        cache();
         // Instantiate the Factory
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         String pesquisa;
         System.out.println("Digite o titulo");
-        do{
-            pesquisa = scan.nextLine();
-            if(cache.get(pesquisa) != null){
-                for (Pagina pagina : cache.get(pesquisa)) {
-                    if (pagina.getPoints() > 50) {
-                        
-                        System.out.println("Id: " + pagina.getId() + ", Titulo: " + pagina.title + ", QdR: "
-                                + pagina.getQdR());
+        do {
+            // INICIANDO A PESQUISA, USANDO A STRING DE PESQUISA PARA SABER SE NO CACHE
+            // EXISTE
+            ArrayList<Pesquisa> Resultados = new ArrayList<>();
+            pesquisa = scan.nextLine().toLowerCase();
+
+            for (int i = 0; i < Paginas.size(); i++) {
+                int points = 0;
+                if (Paginas.get(i).getWords().containsKey(pesquisa)) {
+                    if (Paginas.get(i).getWords().get(pesquisa) > 0)
+                        points = Paginas.get(i).getWords().get(pesquisa).intValue();
+                    Pesquisa atual = new Pesquisa(Paginas.get(i).getId(), Paginas.get(i).getTitle(), points);
+                    Resultados.add(atual);
+                }
+            }
+            if(!Resultados.isEmpty()){
+                Comparator<Pesquisa> relevancia = Collections.reverseOrder(Comparator.comparing(Pesquisa::getPoints));
+                Collections.sort(Resultados, relevancia);
+                for (int i = 0; i < Resultados.size(); i++) {
+                    if (i <= 20) {
+                        System.out.println(
+                                "Id: " + Resultados.get(i).getId() + ", Titulo: " + Resultados.get(i).getTitulo()
+                                        + ", QdP: "
+                                        + Resultados.get(i).getPoints());
                     }
                 }
             }else{
-
-            //PESQUISA
-            
-                try {
-
-                    // parse XML file
-                    DocumentBuilder db = dbf.newDocumentBuilder();
-
-                    Document doc = db.parse(new File(FILENAME));
-                    NodeList list = doc.getElementsByTagName("page");
-                    List<Pagina> paginas = new ArrayList<>();
-                    for (int temp = 0; temp < list.getLength(); temp++) {
-                        Node node = list.item(temp);
-
-                        if (node.getNodeType() == Node.ELEMENT_NODE) {
-
-                            Element element = (Element) node;
-                            // get text
-                            String id = element.getElementsByTagName("id").item(0).getTextContent();
-                            String[] title = element.getElementsByTagName("title").item(0).getTextContent().split(" ");
-                            String[] text = element.getElementsByTagName("text").item(0).getTextContent().toLowerCase().split(" ");
-                            
-                            for (String string : title) {
-                                if(cache.get(string) == null){
-                                    cache.put(string, paginas);
-                                }
-                            }
-                            for (String string : text) {
-                                
-                            }
-                            //Busca
-                           /*   if (text.toLowerCase().contains(pesquisa.toLowerCase())) {
-                                long count = Pattern.compile(pesquisa, Pattern.CASE_INSENSITIVE).matcher(text).results()
-                                        .count();
-                                long points = count;
-                                if (title.toLowerCase().contains(pesquisa.toLowerCase())) {
-                                    points += 10;
-                                }
-                                paginas.add(new Pagina(id, title, count, points));
-                            }*/
-
-                        }
-
-                    }
-                    if (!paginas.isEmpty()) {
-                        Comparator<Pagina> relevancia = Collections.reverseOrder(Comparator.comparing(Pagina::getPoints));
-                        Collections.sort(paginas, relevancia);
-                        cache.put(pesquisa, paginas);
-                        for (Pagina pagina : cache.get(pesquisa)) {
-                            if (pagina.getPoints() > 50) {
-                                
-                                System.out.println("Id: " + pagina.getId() + ", Titulo: " + pagina.title + ", QdR: "
-                                        + pagina.getQdR());
-                            }
-                        }
-                    }
-                    
-                } catch (ParserConfigurationException | SAXException | IOException e) {
-                    e.printStackTrace();
-                }
+                System.out.println("Nada Encontrado");
             }
             System.out.println("Nova Pesquisa?");
-        }while(pesquisa != "sair");
+
+        } while (!pesquisa.equals("sair"));
         scan.close();
 
     }
 
+    public static void cache() {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+
+            DocumentBuilder db = dbf.newDocumentBuilder();
+
+            Document doc = db.parse(new File(FILENAME));
+
+            // COMEÇO PEGANDO AS PAGINAS E TRANSFORMANDO ELAS EM ELEMENTOS DE UMA LISTA DE
+            // PAGINAS(NOS)
+
+            NodeList list = doc.getElementsByTagName("page");
+            // PERCORRENDO PAGINA POR PAGINA PELA LISTA
+            for (int temp = 0; temp < list.getLength(); temp++) {
+                // CONVERTENDO O NO PARA PODER SER UTILIZADO
+                Node node = list.item(temp);
+
+                // SE ELE FOR O TIPO NO, COMEÇA
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+                    // TRANSFORMANDO O NO EM ELEMENTO
+                    Element element = (Element) node;
+
+                    // PEGANDO O ID DA PAGINA
+                    String id = element.getElementsByTagName("id").item(0).getTextContent();
+
+                    // PEGANDO O TITULO E TRANSOFRMANDO NUM VETOR DE STRINGS SEPARADO PELO " "
+                    String[] title = element.getElementsByTagName("title").item(0).getTextContent().split(" ");
+
+                    // PEGANDO O TEXTO E TRANSFORMANDO NUM VETOR DE STRINGS SEPARADOS PELO " "
+                    String[] text = element.getElementsByTagName("text").item(0).getTextContent().toLowerCase()
+                            .split(" ");
+
+                    // CRIANDO O HASHMAP DA PAGINA
+                    HashMap<String, Integer> Words = new HashMap<>();
+
+                    // PARA CADA PALAVRA DO TITULO
+                    for (int value = 0; value < title.length; value++) {
+                        String PALAVRA = title[value].toLowerCase();
+                        if (PALAVRA.length() >= 3) {
+                            if (Words.containsKey(PALAVRA)) {
+                                Words.replace(PALAVRA, Words.get(PALAVRA), Words.get(PALAVRA) + 10);
+                            } else {
+                                Words.put(PALAVRA, 10);
+                            }
+                        }
+                    }
+                    for (int value = 0; value < text.length; value++) {
+                        String PALAVRA = text[value].toLowerCase();
+                        if (PALAVRA.length() >= 3) {
+                            if (Words.containsKey(PALAVRA)) {
+                                Words.replace(PALAVRA, Words.get(PALAVRA), Words.get(PALAVRA) + 1);
+                            } else {
+                                Words.put(PALAVRA, 1);
+                            }
+                        }
+                    }
+                    Pagina pagina = new Pagina(id, element.getElementsByTagName("title").item(0).getTextContent(),
+                            Words);
+                    Paginas.put(temp, pagina);
+                }
+            }
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     static class Pagina {
-        public Pagina(String id, String title, long qdR, long points) {
+        public Pagina(String id, String title, HashMap<String, Integer> Words) {
             this.id = id;
             this.title = title;
-            this.qdR = qdR;
-            this.points = points;
+            this.Words = Words;
         }
 
         private String id;
         private String title;
-        private long qdR;
-        private long points;
+        private HashMap<String, Integer> Words;
 
-        public long getPoints() {
-            return points;
-        }
-
-        public void setPoints(long points) {
-            this.points = points;
+        public HashMap<String, Integer> getWords() {
+            return Words;
         }
 
         public String getId() {
@@ -134,8 +155,29 @@ public class App {
             return title;
         }
 
-        public long getQdR() {
-            return qdR;
+    }
+
+    static class Pesquisa {
+        private String id;
+        private String titulo;
+        private int points;
+
+        public Pesquisa(String id, String titulo, int points) {
+            this.id = id;
+            this.titulo = titulo;
+            this.points = points;
+        }
+
+        public String getTitulo() {
+            return titulo;
+        }
+
+        public int getPoints() {
+            return points;
+        }
+
+        public String getId() {
+            return id;
         }
     }
 
